45a1e76bbac4f783bba7d94ca507fa2b74cd6481
Elisabeth Asbæk Jeppesen <6605388@RTS-365.dk>
2020-12-18 09:24:33 +0100

    add database stuff

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+var express = require("express");
+var app = express();
+var formidable = require("express-formidable");
+var cors = require("cors");
+
+require("./database");
+app.use(cors());
+app.use(formidable());
+
+require("./cheeses.route")(app);
+
+app.listen(process.env.PORT || 6666, function() {
+	console.log("App is running on port", process.env.PORT || 6666);
+});
